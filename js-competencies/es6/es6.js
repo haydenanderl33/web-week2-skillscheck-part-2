7e073a6215a43bb9ba12e0fc0da719ecf0791988
haydenanderl33 <hayden.anderl33@gmail.com>
2021-01-04 16:00:02 -0700

    es6 completo

--- js-competencies/es6/es6.js
+++ js-competencies/es6/es6.js
@@ -12,7 +12,9 @@ function addDucks(arr, ind) {
 
   //WRITE YOUR FOR-LOOP HERE
   //For your iterator, declare it with the let keyword, and name it "i"
-  
+  for(let i = 0; i < arr.length; i++){
+    arr[i] += "Duck"
+  }
 
   //DO NOT TOUCH THIS
   return [arr, ind]
@@ -27,7 +29,7 @@ function addDucks(arr, ind) {
 
 if (true) {
   //CODE HERE
-
+let color = "red"
 
   //DO NOT TOUCH THIS LINE
   getter.get(color)
@@ -39,7 +41,7 @@ if (true) {
 //Assign it the value of "Hello".
 
 //Code here
-
+const greetingsEarthlings = "Hello"
 
 //////////////////PROBLEM 4////////////////////
 //Create a function called madLib that takes in a
@@ -52,7 +54,9 @@ if (true) {
 //look it up, check out the documentation.
 
 //Code here
-
+function madLib(str){
+  return `${str}, my dear Watson.`
+}
 
 //////////////////PROBLEM 5////////////////////
 //Based on the following code, answer the question below:
@@ -69,7 +73,7 @@ var obj = { a: "a", b: "b", c: "c" }
 //used spread operators.
 
 // var answers = ["Example A", "Example B", "Example C", "Example D"]
-// var answers = ["Example B", "Example C", "Example D"]
+var answers = ["Example B", "Example C", "Example D"]
 // var answers = ["Example A", "Example B"]
 // var answers = ["Example C", "Example D"]
 
@@ -80,7 +84,9 @@ var obj = { a: "a", b: "b", c: "c" }
 //string without spaces.
 
 //Code here
-
+const cleanUp = (str) => {
+  return str.replace(/\s+/g,'')
+}
 
 //////////////////PROBLEM 7////////////////////
 //Finish the function below. Assume that the
@@ -91,7 +97,8 @@ var obj = { a: "a", b: "b", c: "c" }
 function whatOcean(obj) {
 
   //Code here
-  
+  const {ocean} = obj
+  return ocean
 }
 
 //////////////////PROBLEM 8////////////////////
@@ -104,4 +111,7 @@ function whatOcean(obj) {
 //plus the last element of the rest parameter, like so:
 //"spaghetti & liver with onions"
 
+const leftovers = (str,...rest) =>{
+  return str + " & " + rest[rest.length-1]
+}
 
